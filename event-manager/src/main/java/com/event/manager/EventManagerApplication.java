215c8880972cd2db0065130e5b398d41a9fba5ce
diegoFranciscoo <diegofrancisco.contato@gmail.com>
2024-09-24 21:47:11 -0300

    eureka service discovery added

--- event-manager/src/main/java/com/event/manager/EventManagerApplication.java
+++ event-manager/src/main/java/com/event/manager/EventManagerApplication.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Events Manager API", version = "2", description = "an event manager api"))
 @EnableFeignClients
+@EnableDiscoveryClient
 public class EventManagerApplication {
 
 	public static void main(String[] args) {
